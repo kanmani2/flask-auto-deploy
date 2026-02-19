9e53a31729b8ac652f7a7dfadbc006749309c366
kanmani M <kanmani3153@gmail.com>
2026-02-19 14:11:50 +0530

    fixing ec2

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-# @app.route("/registration")
-# def registration():
-#     return render_template("registration.html")
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
